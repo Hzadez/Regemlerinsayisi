195d13a2184d9af466ee54e7663bc6a980ccabb3
Hzadez <hzadez63@gmail.com>
2024-10-17 11:45:29 +0400

    Yeni

---
+++ Example(3)/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Example_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir regem girin: ");
+            int regem = Convert.ToInt32(Console.ReadLine());
+            int sayan = 0;
+            do
+            {
+                regem /= 10;
+                sayan++;
+
+            } while (regem != 0);
+            Console.WriteLine(sayan);
+        }
+    }
+}
